bfa07400d165e344daa963b8880adff5c1fdae7d
huyha85 <luan@tklarryonline.me>
2013-07-08 18:13:37 +0700

    Moving on to Part 1.4

--- tkdemo/app/controllers/say_controller.rb
+++ tkdemo/app/controllers/say_controller.rb
@@ -1,6 +1,12 @@
 class SayController < ApplicationController
   def hello
     @time = Time.now
+    @result = say_goodnight('Luan')
+  end
+
+  def say_goodnight(name)
+    result = 'Good night, ' + name
+    return result
   end
 
   def goodbye
