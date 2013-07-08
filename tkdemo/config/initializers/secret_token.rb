a38a36eadbb1b4311fb4de049797438b9de126d9
huyha85 <luan@tklarryonline.me>
2013-07-08 15:57:47 +0700

    Initial

---
+++ tkdemo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tkdemo::Application.config.secret_token = '771551db7ede45607ebf0fb4ae004a45f740a112fdffd83b9205d0bdcb0a1d6a0ace90842bb7d066b6a5935376596df710d9979d6d1965c865cc959e4f01c5c8'
