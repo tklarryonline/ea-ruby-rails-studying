e4303d28f5217c19f503a49fcf58b9a2e3388c0f
tklarryonline <luan@tklarryonline.me>
2013-07-09 17:19:18 +0700

    My first class

--- ruby/hello-world.rb
+++ ruby/hello-world.rb
@@ -1,1 +1,24 @@
-puts 'Hello world'
+#!/usr/bin/ruby
+# My god this is my first ruby-shell program
+
+=begin
+and also my first block comment too!
+=end
+class Greeter
+  def initialize name
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def name= new_name
+    @name = new_name
+  end
+end
+
+g = Greeter.new("Larry")
+puts g.name
+g.name = "Luan"
+puts g.name
