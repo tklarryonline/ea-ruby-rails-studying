1eab486a183119d89f763c780830473fedf1ded0
tklarryonline <luan@tklarryonline.me>
2013-07-15 17:42:46 +0700

    Add relationship to Product and Line_Item

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -11,13 +11,12 @@ class Product < ActiveRecord::Base
   end
 
   private
-    # ensure that there are no line items referencing this product
-    def ensure_not_referenced_by_any_line_item
-      if line_items.empty?
-        return true
-      else
-        errors.add(:base, 'Line Items present')
-        return false
-      end
+  # ensure that there are no line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
     end
+    errors.add(:base, 'Line Items present')
+    return false
+  end
 end
