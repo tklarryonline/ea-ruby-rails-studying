42793cd6b327f7ea632cbc4d15702cc652f25b45
tklarryonline <luan@tklarryonline.me>
2013-07-18 17:58:49 +0700

    Migrated to combine items in cart

--- depot/db/migrate/20130718104650_combine_items_in_cart.rb
+++ depot/db/migrate/20130718104650_combine_items_in_cart.rb
@@ -1,4 +1,24 @@
 class CombineItemsInCart < ActiveRecord::Migration
   def change
   end
+
+  def up
+    # replace multiple items for a single product in a cart with a single item
+    Cart.all.each do |cart|
+      # count the number of each product in the cart
+      sums = cart.line_items.group(:product_id).sum(:quantity)
+
+      sums.each do |product_id, quantity|
+        if quantity > 1
+          # remove individual items
+          cart.line_items.where(product_id: product_id).delete_all
+
+          # then replace with a single item
+          item = cart.line_items.build(product_id: product_id)
+          item.quantity = quantity
+          item.save!
+        end
+      end
+    end
+  end
 end
